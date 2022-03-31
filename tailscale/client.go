bc07744f140280e11268053f1fa3eaee9f1824f0
DeedleFake <deedlefake@users.noreply.github.com>
2022-03-31 17:09:29 -0400

    tailscale: add support for running commands as root

--- tailscale/client.go
+++ tailscale/client.go
@@ -8,11 +8,20 @@ import (
 )
 
 type Client struct {
+	Sudo    string
 	Command string
 }
 
-func (cli Client) run(ctx context.Context, args ...string) (string, error) {
-	cmd := exec.CommandContext(ctx, cli.Command, args...)
+func (cli Client) command(sudo bool, args []string) (string, []string) {
+	if !sudo || (cli.Sudo == "") {
+		return cli.Command, args
+	}
+	return cli.Sudo, append([]string{cli.Command}, args...)
+}
+
+func (cli Client) run(ctx context.Context, sudo bool, args ...string) (string, error) {
+	command, args := cli.command(sudo, args)
+	cmd := exec.CommandContext(ctx, command, args...)
 
 	var out strings.Builder
 	cmd.Stdout = &out
@@ -23,7 +32,7 @@ func (cli Client) run(ctx context.Context, args ...string) (string, error) {
 }
 
 func (cli Client) Status(ctx context.Context) (bool, error) {
-	_, err := cli.run(ctx, "status")
+	_, err := cli.run(ctx, false, "status")
 	if err != nil {
 		var exit *exec.ExitError
 		if errors.As(err, &exit) {

f97f8fdcff706ee81e8a35cbe1368525411c6820
DeedleFake <deedlefake@users.noreply.github.com>
2022-09-11 12:04:06 -0400

    cmd/gtkbuildergen: add a few more UI definitions

--- cmd/gtkbuildergen/uidef.go
+++ cmd/gtkbuildergen/uidef.go
@@ -3,19 +3,47 @@ package main
 import "encoding/xml"
 
 type UIDef struct {
-	XMLName  xml.Name `xml:"interface"`
+	XMLName xml.Name `xml:"interface"`
+
 	Requires []UIDefRequires
 	Objects  []UIDefObject
+	Menus    []UIDefMenu
 }
 
 type UIDefRequires struct {
 	XMLName xml.Name `xml:"requires"`
-	Lib     string   `xml:"lib,attr"`
-	Version string   `xml:"version,attr"`
+
+	Lib     string `xml:"lib,attr"`
+	Version string `xml:"version,attr"`
 }
 
 type UIDefObject struct {
 	XMLName xml.Name `xml:"object"`
-	Class   string   `xml:"class,attr"`
-	ID      string   `xml:"id,attr"`
+
+	Class string `xml:"class,attr"`
+	ID    string `xml:"id,attr"`
+
+	Properties []UIDefProperty
+	Children   []UIDefChild
+}
+
+type UIDefProperty struct {
+	XMLName xml.Name `xml:"property"`
+
+	Name  string `xml:"name,attr"`
+	Value string `xml:",chardata"`
+}
+
+type UIDefChild struct {
+	XMLName xml.Name `xml:"child"`
+
+	Type string `xml:"type,attr"`
+
+	Object UIDefObject
+}
+
+type UIDefMenu struct {
+	XMLName xml.Name `xml:"menu"`
+
+	ID string `xml:"id,attr"`
 }

900aa155bb2d48386ee655fc6498d2f9fb4d1ca7
DeedleFake <deedlefake@users.noreply.github.com>
2022-10-25 14:15:24 -0400

    internal/xslices: add `Subtract()`

--- internal/xslices/xslices.go
+++ internal/xslices/xslices.go
@@ -1,5 +1,7 @@
 package xslices
 
+import "golang.org/x/exp/slices"
+
 // Partition returns two slices, t and f. t contains all elements of s
 // for which pred() returns true, and f contains all elements for
 // which pred() returns false.
@@ -16,4 +18,15 @@ func Partition[E any, S ~[]E](s S, pred func(E) bool) (t, f S) {
 	}
 
 	return t, f
+}
+
+// Subtract returns x minus any elements that are in y.
+func Subtract[E comparable, X ~[]E, Y ~[]E](x X, y Y) X {
+	for i := 0; i < len(x); i++ {
+		if slices.Contains(y, x[i]) {
+			x = slices.Delete(x, i, 1)
+			i--
+		}
+	}
+	return x
 }

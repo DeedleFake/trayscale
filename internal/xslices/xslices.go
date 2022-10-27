0189f2b5b1a5dd8e8ec8a9601fd563171e03de44
DeedleFake <deedlefake@users.noreply.github.com>
2022-10-26 21:26:59 -0400

    internal/xslices: replace `Subtract()` with `Filter()`

--- internal/xslices/xslices.go
+++ internal/xslices/xslices.go
@@ -1,7 +1,5 @@
 package xslices
 
-import "golang.org/x/exp/slices"
-
 // Partition returns two slices, t and f. t contains all elements of s
 // for which pred() returns true, and f contains all elements for
 // which pred() returns false.
@@ -20,13 +18,14 @@ func Partition[E any, S ~[]E](s S, pred func(E) bool) (t, f S) {
 	return t, f
 }
 
-// Subtract returns x minus any elements that are in y.
-func Subtract[E comparable, X ~[]E, Y ~[]E](x X, y Y) X {
-	for i := 0; i < len(x); i++ {
-		if slices.Contains(y, x[i]) {
-			x = slices.Delete(x, i, 1)
-			i--
+// Filter creates a new slice containing only the values in s for
+// which keep(value) returns true.
+func Filter[E any, S ~[]E](s S, keep func(E) bool) S {
+	r := make(S, 0, len(s))
+	for _, v := range s {
+		if keep(v) {
+			r = append(r, v)
 		}
 	}
-	return x
+	return r
 }

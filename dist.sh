dede8af4a4bbe6b5c6d11938ebdf75fe3c5e1b66
DeedleFake <DeedleFake@users.noreply.github.com>
2023-03-30 02:15:04 -0400

    meta: add a build/install script to make packaging easier (#49)
    * meta: add a simple build/install script
    
    * meta: better output from the build script
    
    * meta: make install script a lot more flexible
    
    * meta: add `--always` flag to `git describe`

---
+++ dist.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/bash
+
+# This script is intended to help with correct packaging of Trayscale.
+# It is not intended for general usage. If you'd like to install
+# Trayscale manually, you can read through the installation section of
+# this script to see where various files are put.
+
+_usage() {
+	echo "Usage: $0 <build | install> [options]"
+	echo
+	echo "Modes:"
+	echo "  build: build the binary with embedded version information"
+	echo "    Usage: build [version]"
+	echo "  install: install the binary and other files into a directory tree"
+	echo "    Usage: install <destination directory>"
+	exit 2
+}
+
+_build() {
+	version="$1"
+	if [ -z "$version" ]; then
+		version="$(git describe --tags --always)"
+	fi
+
+	echo "Building $version"
+	go build -v -trimpath -ldflags="-X 'deedles.dev/trayscale/internal/version.version=$version'" -o trayscale ./cmd/trayscale
+}
+
+_install() {
+	dstdir="$1"
+	if [ -z "$dstdir" ]; then
+		_usage
+	fi
+
+	echo "Installing to $dstdir"
+	install -D trayscale "$dstdir/bin/trayscale"
+	install -Dm644 dev.deedles.Trayscale.png "$dstdir/share/icons/hicolor/256x256/apps/dev.deedles.Trayscale.png"
+	install -Dm644 dev.deedles.Trayscale.desktop "$dstdir/share/applications/dev.deedles.Trayscale.desktop"
+	install -Dm644 dev.deedles.Trayscale.metainfo.xml "$dstdir/share/metainfo/dev.deedles.Trayscale.metainfo.xml"
+	install -Dm644 dev.deedles.Trayscale.gschema.xml "$dstdir/share/glib-2.0/schemas/dev.deedles.Trayscale.gschema.xml"
+}
+
+case "$1" in
+	build)
+		_build "$2"
+		;;
+	install)
+		_install "$2"
+		;;
+	--help)
+		_usage
+		;;
+	*)
+		echo "Error: Unknown mode $1"
+		_usage
+		;;
+esac

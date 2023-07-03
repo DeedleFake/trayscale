0936db0436f8f8ba35255b62217cfe7641e41583
Szymon Łągiewka <slagiewka@users.noreply.github.com>
2023-07-03 20:18:52 +0200

    cmd/trayscale: add abstraction to manage tray (#69)
    * cmd/trayscale: add abstraction to manage tray
    
    * cmd/trayscale: rename SetIcon to SetOnlineStatus
    
    * cmd/trayscale: move status icon (w/ embeds) to systray.go
    
    * cmd/trayscale: rename Notify* to *Chan getters

--- cmd/trayscale/systray.go
+++ cmd/trayscale/systray.go
@@ -1,6 +1,60 @@
 package main
 
-import "fyne.io/systray"
+import (
+	_ "embed"
+
+	"fyne.io/systray"
+)
+
+type tray struct {
+	showItem *systray.MenuItem
+	quitItem *systray.MenuItem
+}
+
+func initTray(online bool) *tray {
+	systray.SetIcon(statusIcon(online))
+	systray.SetTitle("Trayscale")
+
+	showWindow := systray.AddMenuItem("Show", "")
+	systray.AddSeparator()
+	quit := systray.AddMenuItem("Quit", "")
+
+	return &tray{
+		showItem: showWindow,
+		quitItem: quit,
+	}
+}
+
+func (t *tray) QuitChan() <-chan struct{} {
+	return t.quitItem.ClickedCh
+}
+
+func (t *tray) ShowChan() <-chan struct{} {
+	return t.showItem.ClickedCh
+}
+
+func (t *tray) SetOnlineStatus(online bool) {
+	if t == nil {
+		return
+	}
+
+	systray.SetIcon(statusIcon(online))
+}
+
+var (
+	//go:embed status-icon-active.png
+	statusIconActive []byte
+
+	//go:embed status-icon-inactive.png
+	statusIconInactive []byte
+)
+
+func statusIcon(online bool) []byte {
+	if online {
+		return statusIconActive
+	}
+	return statusIconInactive
+}
 
 var systrayExit = make(chan func(), 1)
 

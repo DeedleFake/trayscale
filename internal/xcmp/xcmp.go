1c91e135afc1c754620520a3da5957e2da990e54
DeedleFake <deedlefake@users.noreply.github.com>
2023-08-27 00:12:51 -0400

    internal/xcmp: implement `Or()` using `xiter.Find()`

--- internal/xcmp/xcmp.go
+++ internal/xcmp/xcmp.go
@@ -1,13 +1,11 @@
 package xcmp
 
+import "deedles.dev/xiter"
+
 // Or is a basic implementation of the proposed [cmp.Or] function.
 //
 // TODO: Remove when cmp.Or is added, maybe in Go 1.22.
 func Or[T comparable](vals ...T) (r T) {
-	for _, v := range vals {
-		if v != r {
-			return v
-		}
-	}
+	r, _ = xiter.Find(xiter.Slice(vals), func(v T) bool { return v != r })
 	return r
 }

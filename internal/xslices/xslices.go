a8d36f3a873a56fc4ade691716f5a390fcb96315
DeedleFake <deedlefake@users.noreply.github.com>
2024-08-20 15:39:13 -0400

    internal/xslices: remove unused functions

--- internal/xslices/xslices.go
+++ internal/xslices/xslices.go
@@ -2,37 +2,6 @@ package xslices
 
 import "slices"
 
-// Partition returns two slices, t and f. t contains all elements of s
-// for which pred() returns true, and f contains all elements for
-// which pred() returns false.
-func Partition[E any, S ~[]E](s S, pred func(E) bool) (t, f S) {
-	t = make(S, 0, len(s))
-	f = make(S, 0, len(s))
-
-	for _, v := range s {
-		if pred(v) {
-			t = append(t, v)
-			continue
-		}
-		f = append(f, v)
-	}
-
-	return t, f
-}
-
-// Filter removes any elements from s for which keep(element) is
-// false.
-func Filter[E any, S ~[]E](s S, keep func(E) bool) S {
-	to := 0
-	for _, v := range s {
-		if keep(v) {
-			s[to] = v
-			to++
-		}
-	}
-	return s[:to]
-}
-
 // ChunkBy returns a slice of subsequent subsclies of s such that each
 // subslice is a continuous run of elements for which val(element)
 // returns the same value. In other words, a new chunk starts each

90641afb26568e51a2d1ced1c89141b71647670c
DeedleFake <deedlefake@users.noreply.github.com>
2025-05-17 23:50:04 -0400

    internal/listmodels: fix some bugs in `Bind()`

--- internal/listmodels/listmodels.go
+++ internal/listmodels/listmodels.go
@@ -11,7 +11,7 @@ import (
 	"github.com/diamondburned/gotk4/pkg/gtk/v4"
 )
 
-func convertObject[T any](obj *glib.Object) T {
+func Convert[T any](obj *glib.Object) T {
 	if v, ok := obj.Cast().(T); ok {
 		return v
 	}
@@ -33,7 +33,7 @@ func Objects(list gio.ListModeller) iter.Seq2[uint, *glib.Object] {
 func Values[T any](list gio.ListModeller) iter.Seq2[uint, T] {
 	return func(yield func(uint, T) bool) {
 		for i, obj := range Objects(list) {
-			if !yield(i, convertObject[T](obj)) {
+			if !yield(i, Convert[T](obj)) {
 				return
 			}
 		}
@@ -53,7 +53,7 @@ func Backward(list gio.ListModeller) iter.Seq2[uint, *glib.Object] {
 func ValuesBackward[T any](list gio.ListModeller) iter.Seq2[uint, T] {
 	return func(yield func(uint, T) bool) {
 		for i, obj := range Backward(list) {
-			if !yield(i, convertObject[T](obj)) {
+			if !yield(i, Convert[T](obj)) {
 				return
 			}
 		}
@@ -107,7 +107,7 @@ func Update[T comparable](m *gioutil.ListModel[T], s iter.Seq[T]) {
 func Index[T any](m gio.ListModeller, f func(T) bool) (uint, bool) {
 	length := m.NItems()
 	for i := uint(0); i < length; i++ {
-		if f(convertObject[T](m.Item(i))) {
+		if f(Convert[T](m.Item(i))) {
 			return i, true
 		}
 	}
@@ -116,32 +116,31 @@ func Index[T any](m gio.ListModeller, f func(T) bool) (uint, bool) {
 
 func BindListBox[T any](lb *gtk.ListBox, m gio.ListModeller, f func(T) gtk.Widgetter) {
 	lb.BindModel(m, func(obj *glib.Object) gtk.Widgetter {
-		return f(convertObject[T](obj))
+		return f(Convert[T](obj))
 	})
 }
 
-func Bind[T any](
-	add func(int, gtk.Widgetter),
-	remove func(int, gtk.Widgetter),
+func Bind[T, V any](
 	m gio.ListModeller,
-	f func(T) gtk.Widgetter,
+	f func(T) V,
+	remove func(uint, V),
+	add func(uint, V),
 ) func() {
-	widgets := make([]gtk.Widgetter, 0, m.NItems())
+	vals := make([]V, 0, m.NItems())
 	h := m.ConnectItemsChanged(func(index, removed, added uint) {
-		for i, w := range widgets[index : index+removed] {
-			remove(int(index)+i, w)
+		for i, w := range vals[index : index+removed] {
+			remove(index+uint(i), w)
 		}
 
-		new := make([]gtk.Widgetter, 0, added)
-		for i := index; i < added; i++ {
+		newVals := make([]V, 0, added)
+		for i := index; i < index+added; i++ {
 			item := m.Item(i)
-			new = append(new, f(convertObject[T](item)))
-		}
-		widgets = slices.Replace(widgets, int(index), int(removed), new...)
+			w := f(Convert[T](item))
+			add(index+uint(i), w)
 
-		for i, w := range new {
-			add(int(index)+i, w)
+			newVals = append(newVals, w)
 		}
+		vals = slices.Replace(vals, int(index), int(index+removed), newVals...)
 	})
 
 	return func() {

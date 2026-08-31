c41b0e2687dd309dc6ccace65a86f74d9bfc7bc3
DeedleFake <deedlefake@users.noreply.github.com>
2026-08-31 18:08:43 -0400

    internal/ui: add an extra guard

--- internal/ui/peerpage.go
+++ internal/ui/peerpage.go
@@ -305,6 +305,9 @@ func (page *PeerPage) Update(s tsutil.Status) bool {
 }
 
 func peerName(peer tailcfg.NodeView) string {
+	if !peer.Valid() {
+		return ""
+	}
 	return peer.DisplayName(true)
 }
 
